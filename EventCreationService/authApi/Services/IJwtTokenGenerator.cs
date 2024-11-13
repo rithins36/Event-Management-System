ca450ab06fe4d6b1f26f49029724ae166fabbe1f
vishodhana <84466700+vishodhana@users.noreply.github.com>
2024-11-13 19:48:13 +0530

    services

---
+++ EventCreationService/authApi/Services/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using authApi.model;
+using Microsoft.AspNetCore.Identity;
+
+namespace authApi.Services
+{
+    public interface IJwtTokenGenerator
+    {
+        Task<string> GenerateToken(ApplicationUser applicationUser, UserManager<ApplicationUser> userManager);
+    }
+}
---
+++ EventCreationService/authApi/model/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace authApi.model.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+    }
+}
